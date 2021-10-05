5a8c59ad6ce088cedb36605c26158f6f242515d2
Daniel Danis <daniel.danis@jax.org>
2021-10-05 15:41:10 -0400

    Implement `CoordinateSystem.end_delta`, use simple equals instead of matching, add constants

--- src/bound.rs
+++ src/bound.rs
@@ -1,28 +1,33 @@
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum Bound {
     OPEN,
     CLOSED
 }
 impl Bound {
     pub fn is_open(&self) -> bool {
-        match self {
-            Bound::OPEN => {
-                true
-            },
-            Bound::CLOSED => {
-                false
-            }
-        }
+        // TODO - simple equals?
+        self == &Bound::OPEN
+        // self.eq(&Bound::OPEN)
+        // match self {
+        //     Bound::OPEN => {
+        //         true
+        //     },
+        //     Bound::CLOSED => {
+        //         false
+        //     }
+        // }
     }
 
     pub fn is_closed(&self) -> bool {
-        match self {
-            Bound::OPEN => {
-                false
-            },
-            Bound::CLOSED => {
-                true
-            }
-        }
+        // TODO - simple equals?
+        self == &Bound::CLOSED
+        // match self {
+        //     Bound::OPEN => {
+        //         false
+        //     },
+        //     Bound::CLOSED => {
+        //         true
+        //     }
+        // }
     }
 }
