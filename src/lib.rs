138f220b93ec23a350068003af07eaba5662523b
iimpulse <mgdlxd@gmail.com>
2023-04-08 22:23:47 -0600

    creating stranded trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ mod contig;
 mod err;
 mod default;
 mod strand;
+mod stranded;
 
 // TODO - think about API structure
 pub use self::bound::*;
 pub use self::coordinate_system::*;
 pub use self::contig::*;
 pub use self::err::*;
+pub use self::strand::*;
